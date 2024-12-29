21cb294eba8a66eab1927132990e1f2053e11efe
Rubaeid-Sanjid <rubaeid.sanjid@gmail.com>
2024-12-30 01:35:17 +0600

    header added

---
+++ includes/header.php
@@ -0,0 +1,72 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+<header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+        <svg class="bi me-2" width="40" height="32">
+            <use xlink:href="#bootstrap"></use>
+        </svg>
+        <span class="fs-4">QuickE-Shop</span>
+    </a>
+
+    <ul class="nav nav-pills">
+        <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Shop</a></li>
+        <li class="nav-item">
+            <select class="nav-link" name="" id="">
+                <option value="">Categories</option>
+                <option value="">Fashion</option>
+                <option value="">Electronics</option>
+            </select>
+        </li>
+        <li class="nav-item"><a href="#" class="nav-link">About Us</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Contact</a></li>
+    </ul>
+
+    <div class="col-md-3 text-end">
+        <button type="button" class="btn btn-outline-primary me-2">Login</button>
+        <button type="button" class="btn btn-primary">Sign-up</button>
+    </div>
+
+    <div class="dropdown text-end">
+        <a href="#" class="d-block link-body-emphasis text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+            <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" class="rounded-circle">
+        </a>
+        <ul class="dropdown-menu text-small" style="">
+            <li><a class="dropdown-item" href="#">New project...</a></li>
+            <li><a class="dropdown-item" href="#">Settings</a></li>
+            <li><a class="dropdown-item" href="#">Profile</a></li>
+            <li>
+                <hr class="dropdown-divider">
+            </li>
+            <li><a class="dropdown-item" href="#">Sign out</a></li>
+        </ul>
+    </div>
+</header>
+
+<header>
+    <nav>
+        <div>
+            <h1></h1>
+        </div>
+
+        <ul class="">
+            <li>Home</li>
+            <li></li>
+            <li>
+
+            </li>
+            <li></li>
+            <li></li>
+        </ul>
+
+        <div>
+            <!-- icons -->
+            <!-- Cart, Wishlist, Search, and User Account -->
+            <ul>
+                <li>Cart</li>
+                <li>Wishlist</li>
+                <li>User</li>
+            </ul>
+        </div>
+    </nav>
+</header>
