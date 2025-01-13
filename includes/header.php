6a46ad7f958b32ad9230262b5829e32fcf3e76c3
Rubaeid-Sanjid <rubaeid.sanjid@gmail.com>
2025-01-14 01:23:22 +0600

    changed navbar and hero section

--- includes/header.php
+++ includes/header.php
@@ -1,72 +1,19 @@
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
-<header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-        <svg class="bi me-2" width="40" height="32">
-            <use xlink:href="#bootstrap"></use>
-        </svg>
-        <span class="fs-4">QuickE-Shop</span>
-    </a>
-
-    <ul class="nav nav-pills">
-        <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">Shop</a></li>
-        <li class="nav-item">
-            <select class="nav-link" name="" id="">
-                <option value="">Categories</option>
-                <option value="">Fashion</option>
-                <option value="">Electronics</option>
-            </select>
-        </li>
-        <li class="nav-item"><a href="#" class="nav-link">About Us</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">Contact</a></li>
-    </ul>
-
-    <div class="col-md-3 text-end">
-        <button type="button" class="btn btn-outline-primary me-2">Login</button>
-        <button type="button" class="btn btn-primary">Sign-up</button>
-    </div>
-
-    <div class="dropdown text-end">
-        <a href="#" class="d-block link-body-emphasis text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-            <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" class="rounded-circle">
-        </a>
-        <ul class="dropdown-menu text-small" style="">
-            <li><a class="dropdown-item" href="#">New project...</a></li>
-            <li><a class="dropdown-item" href="#">Settings</a></li>
-            <li><a class="dropdown-item" href="#">Profile</a></li>
-            <li>
-                <hr class="dropdown-divider">
-            </li>
-            <li><a class="dropdown-item" href="#">Sign out</a></li>
-        </ul>
-    </div>
-</header>
-
 <header>
-    <nav>
-        <div>
-            <h1></h1>
+    <div class="container header-content">
+        <div class="logo">
+            <span style="background-color: #262b34; color: white;">
+                QuickE-
+            </span>
+            <span style="background-color: white; color: #262b34; padding: 1px;">Shop</span>
         </div>
-
-        <ul class="">
-            <li>Home</li>
-            <li></li>
-            <li>
-
-            </li>
-            <li></li>
-            <li></li>
-        </ul>
-
-        <div>
-            <!-- icons -->
-            <!-- Cart, Wishlist, Search, and User Account -->
+        <nav>
             <ul>
-                <li>Cart</li>
-                <li>Wishlist</li>
-                <li>User</li>
+                <li><a href="#">HOME</a></li>
+                <li><a href="#">SHOP</a></li>
+                <li><a href="#">ACCOUNT</a></li>
+                <li><a href="#">ABOUT US</a></li>
+                <li><a href="#">CONTACT</a></li>
             </ul>
-        </div>
-    </nav>
+        </nav>
+    </div>
 </header>
---
+++ includes/home/hero.php
@@ -0,0 +1,16 @@
+<section class="hero">
+  <div class="hero-content">
+    <div>
+      <h1 style="font-size: 3rem;">Welcome to QuickE-Shop</h1>
+      <p>Shop Smart, Shop Quick!</p>
+    </div>
+
+    <div>
+      <img src="../../images/boy.png" alt="" style="height: 300px; width: 300px;">
+    </div>
+  </div>
+  <div class="cta">
+    <button>Shop Now</button>
+    <button>Explore Categories</button>
+  </div>
+</section>
